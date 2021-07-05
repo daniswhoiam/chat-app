a4ad252004bfc1e118b03ffeecefd05b6f68d238
daniswhoiam <daniil.belazovschi@gmail.com>
2021-07-05 12:38:04 +0200

    Refactored chat into class component

--- components/Chat.js
+++ components/Chat.js
@@ -1,54 +1,61 @@
 // Import packages
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import { View, Platform, KeyboardAvoidingView } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
 
-/**
- * This is the view that enables the main function of the app, which is chatting.
- *
- * @param {Object} props Contains user's name, chosen color, and navigation
- * @returns Rendered Chat View
- */
-export default function Chat(props) {
-  const { color, name } = props.route.params;
-  
-  const [messages, setMessages] = useState([]);
-  // Functional Component Equivalent of componentDidMount()
-  useEffect(() => {
-    setMessages([
-      {
-        _id: 2,
-        text: 'Hello developer',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any'
-        }
-      },
-      {
-        _id: 1,
-        text: `${name} has entered the chat`,
-        createdAt: new Date(),
-        system: true
-      }
-    ]);
-  }, []);
+export default class Chat extends Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: []
+    };
+  }
 
-  const onSend = (newMessages = []) => {
-    setMessages(GiftedChat.append(messages, newMessages));
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 2,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any'
+          }
+        },
+        {
+          _id: 1,
+          text: `${this.props.route.params.name} has entered the chat`,
+          createdAt: new Date(),
+          system: true
+        }
+      ]
+    });
+  }
+
+  onSend = (newMessages = []) => {
+    this.setState(previousState => {
+      return {
+        messages: GiftedChat.append(previousState.messages, newMessages)
+      };
+    });
   };
 
-  return (
-    <View style={{ flex: 1, backgroundColor: `${color}` }}>
-      <GiftedChat
-        messages={messages}
-        onSend={newMessages => onSend(newMessages)}
-        user={{ _id: 1 }}
-      />
-      {Platform.OS === 'android' ? (
-        <KeyboardAvoidingView behavior="height" />
-      ) : null}
-    </View>
-  );
+  render() {
+    const color = this.props.route.params.color;
+
+    return (
+      <View style={{ flex: 1, backgroundColor: `${color}` }}>
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={newMessages => this.onSend(newMessages)}
+          user={{ _id: 1 }}
+        />
+        {Platform.OS === 'android' ? (
+          <KeyboardAvoidingView behavior="height" />
+        ) : null}
+      </View>
+    );
+  }
 }

c5390cdf1aae1f1a10e966f4a93ec985151eaf78
daniswhoiam <daniil.belazovschi@gmail.com>
2021-07-05 15:44:45 +0200

    Completely refactored Chat component

--- components/Chat.js
+++ components/Chat.js
@@ -1,112 +1,100 @@
 // Import packages
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Platform, KeyboardAvoidingView, Text } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
-import firebase from 'firebase';
+import { db, auth } from '../firebase';
 
-export default class Chat extends Component {
-  constructor() {
-    super();
-    this.state = {
-      messages: [],
-      uid: 0,
-      loggedInText: 'Please wait, you are getting logged in'
-    };
+/**
+ * This is the view that enables the main function of the app, which is chatting.
+ *
+ * Inspired by https://github.com/jjla26
+ *
+ * @param {Object} props Contains user's name, chosen color, and navigation
+ * @returns Rendered Chat View
+ */
+export default function Chat(props) {
+  // Initiate state
+  const [user, setUser] = useState(null);
+  const [messages, setMessages] = useState([]);
 
-    if (!firebase.apps.length) {
-      firebase.initializeApp({
-        apiKey: 'AIzaSyDQPor7wk0hOlrSBe6DOeM-zY4B7mOi4TI',
-        authDomain: 'test-4a175.firebaseapp.com',
-        projectId: 'test-4a175',
-        storageBucket: 'test-4a175.appspot.com',
-        messagingSenderId: '20091337538',
-        appId: '1:20091337538:web:dde10fb1308cd141c366db'
-      });
-    }
-  }
-
-  componentDidMount() {
-    this.referenceMessages = firebase.firestore().collection('messages');
-
-    this.authUnsubscribe = firebase.auth().onAuthStateChanged(async user => {
+  // Euqivalent to componentDidMount()
+  useEffect(() => {
+    let unsubscribeMessages = () => {};
+    // Authentication of the user
+    let authUnsubscribe = auth.onAuthStateChanged(async user => {
       if (!user) {
-        await firebase.auth().signInAnonymously();
+        try {
+          await auth.signInAnonymously();
+        } catch (error) {
+          console.log(error);
+        }
       }
-      this.setState({
-        uid: user.uid,
-        loggedInText: 'Hello there'
-      });
-
-      this.referenceMessagesUser = firebase
-        .firestore()
+      setUser(user.uid);
+      // Establishing connection with firestore
+      unsubscribeMessages = db
         .collection('messages')
-        .where('uid', '==', this.state.uid);
-      this.unsubscribeMessagesUser = this.referenceMessagesUser
-        /* .orderBy('createdAt', 'desc') */
-        .onSnapshot(this.onCollectionUpdate);
+        .orderBy('createdAt', 'desc')
+        .onSnapshot(onCollectionUpdate);
     });
-  }
 
-  componentWillUnmount() {
-    this.authUnsubscribe();
-    this.unsubscribeMessagesUser();
-  }
+    // Equivalent of componentWillUnmount()
+    return () => {
+      authUnsubscribe();
+      unsubscribeMessages();
+    };
+  }, []);
 
-  onSend = (newMessages = []) => {
-    this.setState(
-      previousState => {
-        return {
-          messages: GiftedChat.append(previousState.messages, newMessages)
-        };
-      },
-      () => this.addMessage()
-    );
-  };
-
-  addMessage = () => {
-    const newMessage = this.state.messages[0];
-    this.referenceMessages.add({
-      createdAt: new Date(),
-      system: false,
-      text: newMessage.text,
+  /**
+   * Adds the new message to the database
+   *
+   * @param {Array} newMessage Array containing the new message that is being sent
+   */
+  const onSend = (newMessage = []) => {
+    const message = {
+      _id: newMessage[0]._id,
+      createdAt: newMessage[0].createdAt,
+      text: newMessage[0].text,
       user: {
-        _id: this.state.uid,
-        name: this.props.route.params.name
+        _id: user,
+        name: props.route.params.name
       }
-    });
+    };
+    db.collection('messages').add(message);
   };
 
-  onCollectionUpdate = querySnapshot => {
-    const messages = [];
+  /**
+   * Update list of messages if new ones appear
+   *
+   * @param {Object} querySnapshot Snapshot of the collection in the database
+   */
+  const onCollectionUpdate = querySnapshot => {
+    const allMessages = [];
     querySnapshot.forEach(doc => {
       let data = doc.data();
-      messages.push({
-        createdAt: data.createdAt,
+      allMessages.push({
+        _id: doc.id,
+        createdAt: data.createdAt.toDate(),
         system: data.system,
         text: data.text,
-        user: data.user
+        user: {
+          _id: data.user._id,
+          name: data.user.name
+        }
       });
     });
-    this.setState({
-      messages
-    });
+    setMessages(allMessages);
   };
 
-  render() {
-    const color = this.props.route.params.color;
-
-    return (
-      <View style={{ flex: 1, backgroundColor: `${color}` }}>
-        <Text>{this.state.loggedInText}</Text>
-        <GiftedChat
-          messages={this.state.messages}
-          onSend={newMessages => this.onSend(newMessages)}
-          /* user={{ _id: 1 }} */
-        />
-        {Platform.OS === 'android' ? (
-          <KeyboardAvoidingView behavior="height" />
-        ) : null}
-      </View>
-    );
-  }
+  return (
+    <View style={{ flex: 1, backgroundColor: `${props.route.params.color}` }}>
+      <GiftedChat
+        messages={messages}
+        onSend={newMessages => onSend(newMessages)}
+        user={{ _id: user }}
+      />
+      {Platform.OS === 'android' ? (
+        <KeyboardAvoidingView behavior="height" />
+      ) : null}
+    </View>
+  );
 }

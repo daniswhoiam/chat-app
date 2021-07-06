7665b3044b5d874911efc21347d2a37802ea00c7
daniswhoiam <daniil.belazovschi@gmail.com>
2021-07-06 18:21:10 +0200

    Added offline functionality

--- components/Chat.js
+++ components/Chat.js
@@ -1,8 +1,10 @@
-// Import packages
-import React, { useState, useEffect } from 'react';
-import { View, Platform, KeyboardAvoidingView, Text } from 'react-native';
-import { GiftedChat } from 'react-native-gifted-chat';
+// Import external packages
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Platform, KeyboardAvoidingView } from 'react-native';
+import { GiftedChat, Bubble, InputToolbar } from 'react-native-gifted-chat';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import NetInfo from '@react-native-community/netinfo';
+// Internal modules
 import { db, auth } from '../firebase';
 
 /**
@@ -14,43 +16,63 @@ import { db, auth } from '../firebase';
  * @returns Rendered Chat View
  */
 export default function Chat(props) {
+  // To be able to avoid useEffects() calls on first Render
+  const firstRender = useRef(true);
   // Initiate state
   const [user, setUser] = useState(null);
   const [messages, setMessages] = useState([]);
+  const [isConnected, setIsConnected] = useState(true);
 
   // Equivalent to componentDidMount()
   useEffect(() => {
-    let unsubscribeMessages = () => {};
-    // Authentication of the user
-    let authUnsubscribe = auth.onAuthStateChanged(async user => {
-      if (!user) {
-        try {
-          await auth.signInAnonymously();
-        } catch (error) {
-          console.log(error);
-        }
-      }
-      setUser(user.uid);
-      // Establishing connection with firestore
-      unsubscribeMessages = db
-        .collection('messages')
-        .orderBy('createdAt', 'desc')
-        .onSnapshot(onCollectionUpdate);
-    });
+    NetInfo.fetch().then(connection => {
+      if (connection.isConnected) {
+        let unsubscribeMessages = () => {};
+        // Authentication of the user
+        let authUnsubscribe = auth.onAuthStateChanged(async user => {
+          if (!user) {
+            try {
+              await auth.signInAnonymously();
+            } catch (error) {
+              console.log(error);
+            }
+          }
+          setUser(user.uid);
+          // Establishing connection with firestore
+          unsubscribeMessages = db
+            .collection('messages')
+            .orderBy('createdAt', 'desc')
+            .onSnapshot(onCollectionUpdate);
+        });
 
-    // Equivalent of componentWillUnmount()
-    return () => {
-      authUnsubscribe();
-      unsubscribeMessages();
-    };
+        // Equivalent of componentWillUnmount()
+        return () => {
+          authUnsubscribe();
+          unsubscribeMessages();
+        };
+      } else {
+        // User is offline, load saved messages
+        setIsConnected(false);
+        getMessages();
+      }
+    });
   }, []);
+
+  /* Save messages to local storage each time that message state changes */
+  useEffect(() => {
+    if (firstRender.current) {
+      firstRender.current = false;
+      return;
+    }
+    saveMessages();
+  }, [messages]);
 
   /**
    * Adds the new message to the database
    *
    * @param {Array} newMessage Array containing the new message that is being sent
    */
-  const onSend = (newMessage = []) => {
+  const onSend = async (newMessage = []) => {
     const message = {
       _id: newMessage[0]._id,
       createdAt: newMessage[0].createdAt,
@@ -60,8 +82,8 @@ export default function Chat(props) {
         name: props.route.params.name
       }
     };
+    // Push message to firestore
     db.collection('messages').add(message);
-    saveMessages();
   };
 
   /**
@@ -87,6 +109,9 @@ export default function Chat(props) {
     setMessages(allMessages);
   };
 
+  /**
+   * Loads saved messages from local storage (if user is offline).
+   */
   const getMessages = async () => {
     let storedMessages = '';
     try {
@@ -97,6 +122,9 @@ export default function Chat(props) {
     }
   };
 
+  /**
+   * Saves messages to local storage.
+   */
   const saveMessages = async () => {
     try {
       await AsyncStorage.setItem('messages', JSON.stringify(messages));
@@ -105,6 +133,9 @@ export default function Chat(props) {
     }
   };
 
+  /**
+   * Allows to delete messages.
+   */
   const deleteMessages = async () => {
     try {
       await AsyncStorage.removeItem('messages');
@@ -120,6 +151,25 @@ export default function Chat(props) {
         messages={messages}
         onSend={newMessages => onSend(newMessages)}
         user={{ _id: user }}
+        renderBubble={props => {
+          return (
+            <Bubble
+              {...props}
+              wrapperStyle={{
+                right: {
+                  backgroundColor: '#000'
+                }
+              }}
+            />
+          );
+        }}
+        renderInputToolbar={props => {
+          // Prevent user from sending messages in offline mode
+          if (!isConnected) {
+          } else {
+            return <InputToolbar {...props} />;
+          }
+        }}
       />
       {Platform.OS === 'android' ? (
         <KeyboardAvoidingView behavior="height" />
